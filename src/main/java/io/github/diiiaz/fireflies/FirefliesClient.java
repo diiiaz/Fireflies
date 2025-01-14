925e1ef61d13fbe0be7c08e1907bfc6c83378804
Diaz <cantonival@gmail.com>
2025-01-14 21:40:13 +0100

    Initial commit

---
+++ src/main/java/io/github/diiiaz/fireflies/FirefliesClient.java
@@ -0,0 +1,12 @@
+package io.github.diiiaz.fireflies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirefliesClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
